6beaf32d8335fad91d3a8067367a2bb911f566e8
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-10-01 23:10:41 +0300

    fix: try 1 red check 8, used sha1

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,32 +1,25 @@
-const crypto = require('crypto');
+const sha1 = require('sha1');
 const dbClient = require('../utils/db');
-
-const sha1 = crypto.createHash('sha1');
 
 class UsersController {
   static async postNew(data, res) {
     const { email, password } = data;
-    if (!email) {
-      res.status(400).send({ error: 'Missing email' });
-      return;
-    }
-    if (!password) {
-      res.status(400).send({ error: 'Missing password' });
-      return;
-    }
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+
+    if (!password) return res.status(400).send({ error: 'Missing password' });
+
     const user = await dbClient.db.collection('users').findOne({ email });
-    if (user) {
-      res.status(400).send({ error: 'Already exist' });
-      return;
-    }
-    const hashedPassword = sha1.update(password).digest('hex');
-    await dbClient.db.collection('users').insertOne({ email, hashedPassword }, (error, result) => {
-      if (error) {
-        res.status(500).send({ error });
-        return;
-      }
-      res.status(201).send({ id: result.insertedId, email });
-    });
+    if (user) return res.status(400).send({ error: 'Already exist' });
+
+    const hashedPassword = sha1(password);
+
+    await dbClient.db.collection('users').insertOne({ email, hashedPassword },
+      (error, result) => {
+        if (error) return res.status(500).send({ error });
+
+        return res.status(201).send({ id: result.insertedId, email });
+      });
+    return true;
   }
 }
 
